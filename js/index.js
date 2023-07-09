55932a24478731f80bd3021169821faef855d412
CoffeeKiller <18320210361@163.com>
2023-07-09 19:42:38 +0800

    update

--- js/index.js
+++ js/index.js
@@ -6,6 +6,18 @@ $(function(){
 });
 
 
+let tecStack = {
+    java:["JavaSE","JavaWeb","JavaGUI","JavaFX","Spring","SpringMVC","MyBatisPlus","SpringBoot","SpringCloud","JVM虚拟机","Maven"],
+    web:["HTML5","CSS3","JavaScript","ES 6","微信小程序","Uni-App","Nodejs","TypeScript","Express","Vue全家桶","ThreeJS","WebPack"],
+    other:["Python","C","C sharp","Unity3D","RPG Maker","LuaScript","Php","Golang","Android"],
+    Extend:["MySQL","MongoDB","Linux","Nginx","Redis","RabbitMQ","Docker","设计模式","数据结构与算法","计算机网络"]
+}
+
+
+const p3swiper = new Swiper(".p3swiper", {
+
+});
+
 $(window).resize(function(){
     autoScrolling();
 });
