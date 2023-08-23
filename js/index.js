0e766fad7904a2ca1c0fb3a5a96f68bba3be7913
CoffeeKiller <18320210361@163.com>
2023-08-23 23:30:26 +0800

    update

--- js/index.js
+++ js/index.js
@@ -6,28 +6,6 @@ $(function(){
 });
 
 
-let tecStack = [
-    ["JavaSE","JavaWeb","JavaGUI","JavaFX","Spring","SpringMVC","MyBatisPlus","SpringBoot","SpringCloud","JVM虚拟机原理","Maven"],
-    ["HTML5","CSS3","JavaScript","ES 6","微信小程序","Uni-App","Nodejs","TypeScript","Express","Vue全家桶","ThreeJS","WebPack"],
-    ["Python","C","C sharp","Unity3D","RPG Maker","LuaScript","Php","Golang","Android"],
-    ["MySQL","MongoDB","Linux","Nginx","Redis","RabbitMQ","Docker","设计模式","数据结构与算法","计算机网络"]
-]
-
-function initTech() {
-    let _html = ``
-    for (const item in tecStack) {
-        _html += `<div class="swiper-slide">`
-        for (const key in tecStack[item]) {
-            _html += `
-                <li class="tech-item">${tecStack[item][key]}</li>
-            `
-        }
-        _html += "</div>"
-    }
-    $('.swiper-wrapper').html(_html)
-}
-
-initTech()
 
 
 const p3swiper = new Swiper(".p3swiper", {
