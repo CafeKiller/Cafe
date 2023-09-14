2f79ee66aa3946651548718a21839e8e03fa3e51
CoffeeKiller <18320210361@163.com>
2023-09-14 21:49:09 +0800

    update 2023年9月14日21:49:06

--- js/context.js
+++ js/context.js
@@ -91,30 +91,43 @@ if(isMobile()){
         afterLoad: function(anchorLink, index){
             switch (index) {
                 case 1:
+                    /* 首页元素加载动画 */
                     gsap.to(".part1-cont div", 0.3, {opacity: 1,y: 60,delay:0.3, stagger:0.3})
                     break
                 case 2:
+                    /* 简介页bef元素加载动画 */
                     $(".part2-icon1, .part2-icon2").addClass("active")
                     break
                 case 3:
+                    /* 技术栈进入动画 */
                     gsap.to(".part3-item", 0.2, {opacity: 1,x: 60,delay:0.15,stagger:0.1,
                                                 onCompleta:()=>{
+                                                        /* 技术栈进度图加载 */
                                                         stackProgressLoad(stack_progress_list)
                                                         $('.part3-icon-1').css({ "animation": "MarioJump .2s 3 linear" })
                                                         $('.part3-icon-2').css({ "animation": "CubeSway .2s 3 linear" })
                                                     }})
                     break
                 case 4:
-                    gsap.to(".swiper-slide", 0.3, {opacity: 1,y: -20,delay:0.2}, 0.15)
-                    gsap.to(".p4swiper-pagination", 0.3, {opacity: 1,y: -20,delay:0.2}, 0.15)
-                    $(".p4swiper .swiper-slide").css({"pointer-events":"all"})
+                    /* 轮播slide 渐渐上浮显示 */
+                    gsap.to(".swiper-slide", 0.3, {opacity: 1, y: -20, delay:0.2, stagger:0.15,
+                                                    onCompleta:()=>{
+                                                            /* 允许点击 */
+                                                            $(".p4swiper .swiper-slide").css({"pointer-events":"all"})
+                                                        }})
+                    /* 轮播pagination 渐渐上浮显示 */
+                    gsap.to(".p4swiper-pagination", 0.3, {opacity: 1, y: -20, delay:0.75})
                     break
                 case 5:
-                    gsap.to(".left-model",0.3, {opacity: 1,x: 80,delay:0.2}, 0.15)
-                    gsap.to(".btom-model",0.2, {opacity: 1,y: -80,delay:0.2}, 0.15,() => {
-                        $(`.part5-cont-item:eq(${part5_current_active})`).addClass("active")
-                        // init_Part5Item_MouseEvent()
-                    })
+                    /* 底部模块加载 */
+                    gsap.to(".left-model",0.3, {opacity: 1, x: 80, delay:0.2, stagger:0.15})
+                    gsap.to(".btom-model",0.2, {opacity: 1, y: -80, delay:0.2, stagger:0.15, 
+                                               onCompleta:()=>{
+                                                    /* 为首个元素添加进入动画 */
+                                                    $(`.part5-cont-item:eq(${part5_current_active})`).addClass("active")
+                                                    // init_Part5Item_MouseEvent()
+                                               }})
+                    /* 内容加载 */
                     gsap.to(".part5-cont-item.cont-model .title",0.5, {opacity: 1,x: -150,delay:0.65})
                     gsap.to(".part5-cont-item.cont-model .content",0.5, {opacity: 1,y: -50,delay:0.65})
             }
